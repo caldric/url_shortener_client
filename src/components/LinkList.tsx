9f1ab14d604a374121483fb889fe579e2fc3e6e7
caldric <cvaldric@gmail.com>
2021-02-11 14:19:45 -0500

    Attach state

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface Link {
+  id: string;
+  url: string;
+}
 
 const LinkList = () => {
+  const [links, setLinks] = useState<Link[]>([]);
+
   return <div>LinkList</div>;
 };
 
