397b7c0b0f1d92b6f907df886a641ee28f0bf151
caldric <cvaldric@gmail.com>
2021-02-11 14:58:56 -0500

    Move links state from LinkList to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LinkForm from './components/LinkForm';
 import LinkList from './components/LinkList';
 
+export interface Link {
+  _id: string;
+  url: string;
+}
+
 const App: React.FC = () => {
+  const [links, setLinks] = useState<Link[]>([]);
   const apiBaseUrl: string = 'http://localhost:8080';
 
   return (
     <div>
       <LinkForm apiBaseUrl={apiBaseUrl} />
-      <LinkList apiBaseUrl={apiBaseUrl} />
+      <LinkList apiBaseUrl={apiBaseUrl} links={links} setLinks={setLinks} />
     </div>
   );
 };
--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-
-interface Link {
-  _id: string;
-  url: string;
-}
+import { Link } from '../App';
 
 interface Props {
   apiBaseUrl: string;
+  links: Link[];
+  setLinks: React.Dispatch<React.SetStateAction<Link[]>>;
 }
 
-const LinkList: React.FC<Props> = ({ apiBaseUrl }) => {
-  const [links, setLinks] = useState<Link[]>([]);
-
+const LinkList: React.FC<Props> = ({ apiBaseUrl, links, setLinks }) => {
   useEffect(() => {
     const getLinks = async (): Promise<void> => {
       const { data } = await axios.get(apiBaseUrl);
