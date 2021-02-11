6a83684ad06dc56c41c74644a1b16b6dd1137057
caldric <cvaldric@gmail.com>
2021-02-11 14:40:57 -0500

    Obtain links from API

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 interface Link {
-  id: string;
+  _id: string;
   url: string;
 }
 
 const LinkList = () => {
   const [links, setLinks] = useState<Link[]>([]);
+  const baseUrl: string = 'http://localhost:8080';
 
-  return <div>LinkList</div>;
+  useEffect(() => {
+    const getLinks = async (): Promise<void> => {
+      const { data } = await axios.get(baseUrl);
+      setLinks(data);
+    };
+
+    getLinks();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {links.map((link) => {
+          const shortenedUrl = `${baseUrl}/${link._id}`;
+          return (
+            <li key={link._id}>
+              <div>
+                <span>{link.url}</span> -{' '}
+                <span>
+                  <a href={shortenedUrl}>{shortenedUrl}</a>
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default LinkList;
